bc22f407a3f7d791c0c7ac7409129e8edc9216df
MilosPolo <tunepologame@gmail.com>
2024-11-20 00:58:19 +0100

    m

--- DZ2/main.cpp
+++ DZ2/main.cpp
@@ -1,18 +1,75 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include<math.h>
 
 using namespace std;
 
-class Node {
+class Data {
 public:
-
-	uint64_t id;
 	string first;
 	string last;
 	string email;
 	uint64_t ad_id;
 
-	Node(uint64_t id, string first, string last, string email, uint64_t ad_id): id(id), first(first), last(last), email(email), ad_id(ad_id) {}
+	Data(string first, string last, string email, uint64_t ad_id): first(first), last(last), email(email), ad_id(ad_id) {}
+};
+
+
+class Node {
+public:
+	uint64_t id;
+	Data* dat;
+	Node* parent;
+	vector<Node*> next;
+	int min, max;
+	bool list;
+
+	Node(uint64_t id, string first, string last, string email, uint64_t ad_id, Node* parent, int m, bool list) : id(id), parent(parent), list(list){
+		dat = new Data(first, last, email, ad_id);
+
+		if(!parent){ //root
+			min = 2;
+			max = 2*floor((2*(double)m - 2)/3);
+		}
+		else {
+			if (list) {
+				min = 0;
+				max = 0;
+			}
+			else {
+				min = ceil((2 * m - 1) / 3);
+				max = m;
+			}
+		}
+		next = vector<Node*>(max);
+	}
+};
+
+class IndexTree {
+public:
+	Node* root;
+	int m;
+
+
+	IndexTree(int m) {
+		this->m = m;
+		root = nullptr;
+	}
+
+	void insert(uint64_t id, string first, string last, string email, uint64_t ad_id) {
+		if (!root) {
+			root = new Node(id, first, last, email, ad_id, nullptr, m, true);
+			return;
+		}
+
+		Node* curr = root;
+		while (curr->next.size() != 0) {
+			curr = curr->findNextByKey(id);
+		}
+		//add value to curr list if possible
+		//if not prelom
+	}
 };
 
 
@@ -23,9 +80,13 @@ void main() {
 	//B* stablo reda m izmedju [3,10]
 
 	int m;
-	cout << "Uneti red stabla: ";
-	cin >> m;
 
+}
 
-	
+void createIndex(int& m) {
+	while (1) {
+		cout << "Uneti red stabla: ";
+		cin >> m;
+		if (m >= 3 || m <= 10) break;
+	}
 }
